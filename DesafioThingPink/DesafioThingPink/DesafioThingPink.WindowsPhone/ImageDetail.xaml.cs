566cecb8e2c2bef3dcc014f0897bf8acf2502dbf
facpatricio <facpatricio@gmail.com>
2014-09-29 18:10:44 +0100

    ImageDetailPage AppBar DONE ImageDetail save to library DONE TODO: Share image on FACEBOOK

--- DesafioThingPink/DesafioThingPink/DesafioThingPink.WindowsPhone/ImageDetail.xaml.cs
+++ DesafioThingPink/DesafioThingPink/DesafioThingPink.WindowsPhone/ImageDetail.xaml.cs
@@ -15,6 +15,11 @@ using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using DesafioThingPink.ViewModels;
 using DesafioThingPink.Models;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -26,6 +31,8 @@ namespace DesafioThingPink
     public sealed partial class ImageDetail : Page
     {
         ImageData image_data;
+        BitmapImage bitmap_image = new BitmapImage();
+
 
         public ImageDetail()
         {
@@ -40,7 +47,8 @@ namespace DesafioThingPink
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             image_data = (e.Parameter as ImageData);
-            Image.Source = new BitmapImage(new Uri(image_data.images.standard_resolution.url, UriKind.Absolute));
+            bitmap_image = new BitmapImage(new Uri(image_data.images.standard_resolution.url, UriKind.Absolute));
+            Image.Source = bitmap_image;
             UsernameText.Text = "@" + image_data.user.username;
             LikesText.Text = image_data.likes.count + " Likes";
 
@@ -53,6 +61,50 @@ namespace DesafioThingPink
         private void BackTapped(object sender, TappedRoutedEventArgs e)
         {
             Frame.GoBack();
+        }
+
+        private async void SaveImageOnCameraRoll_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                // http://stackoverflow.com/questions/25343475/writeablebitmap-savejpeg-missing-for-universal-apps
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(image_data.images.standard_resolution.url);
+                await Task.Run(async () =>
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var ras = new InMemoryRandomAccessStream();
+                        var inputBytes = await response.Content.ReadAsByteArrayAsync();
+                        var writer = new DataWriter(ras);
+                        writer.WriteBytes(inputBytes);
+                        await writer.StoreAsync();
+                        var inputStream = ras.GetInputStreamAt(0);
+
+                        // write the picture into this folder
+                        var storageFile = await KnownFolders.PicturesLibrary.CreateFileAsync(image_data.id + ".jpg", CreationCollisionOption.GenerateUniqueName);
+                        using (var storageStream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+                        {
+                            await RandomAccessStream.CopyAndCloseAsync(inputStream, storageStream.GetOutputStreamAt(0));
+                        }
+                    }
+                });
+                MainPage.ShowMessage("Imagem guardada com sucesso");
+            }
+            catch
+            {
+                MainPage.ShowMessage("Ocorreu um erro ao guardar a imagem");
+            }
+
+
+            
+        }
+
+        private async void ShareOnFacebook_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
